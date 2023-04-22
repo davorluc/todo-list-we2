9fd6436478385449760804e57b5a20590339d26c
davor_l <d.lucic4@hotmail.com>
2023-04-22 16:33:03 +0200

    commit

--- controller/index-controller.js
+++ controller/index-controller.js
@@ -1,6 +1,66 @@
+import {todoService} from "../services/todoService.js";
+
 export class IndexController {
-    index(req, res) {
-        res.render("index", {data: "Hello World", dark: true});
+    showIndex(req, res) {
+        res.type('text/html');
+        res.write("<html>");
+        res.write("<p>Willkommen! Zu der besten Pizzaria auf der Welt!</p>");
+        res.write("<img src='/images/pizza.jpg'>");
+        res.write("<form action='/orders' method='get'><input type='submit' value='Order a Pizza'></form>");
+        res.end("</html>");
+    };
+
+    createOrder(req, res) {
+        res.type('text/html');
+        res.write("<html>");
+        res.write("<p>Was fuer eine Pizze haetten Sie den gerne?</p>");
+        res.write("<form action='/orders' method='post'><input name='name' placeholder='pizza name'><input type='submit' value='Order a Pizza'></form>");
+        res.end("</html>");
+    };
+
+    createPizza(req, res) {
+        console.log("createPizza start");
+        todoService.add(req.body.name, "unkown", function (err, order) {
+            console.log("      callback start");
+
+            res.type('text/html');
+            res.write("<html>");
+            res.write("<p>Erfolgreich!</p>");
+            res.write("<p>Ihre order: " + order.pizzaName + "</p>");
+            res.write("<p>Ihre Nummer: " + order._id + " !</p>");
+            res.write("<p><a href='/orders/" + order._id + "/'>Zeige order an</a></p>");
+            res.end("</html>");
+
+            console.log("      callback end");
+        });
+        console.log("createPizza end");
+    };
+
+    showOrder(req, res) {
+        todoService.get(req.params.id, function (err, order) {
+            res.type('text/html');
+            res.write("<html>");
+            if (order) {
+                res.write("<p>Order-Number: " + order._id + "</p>");
+                res.write("<p>Status: " + order.state + "</p>");
+                if (order.state === "OK") {
+                    res.write("<form action='/orders/" + order._id + "' method='post'><input type='hidden' name='_method'  value='delete'><input type='submit' value='Delete order'></form>");
+                }
+            }
+            res.write("<form action='/' method='get'><input type='submit' value='Zurueck zum start'></form>");
+            res.end("</html>");
+        });
+    };
+
+    deleteOrder(req, res) {
+        todoService.delete(req.params.id, function (err, order) {
+            res.type('text/html');
+            res.write("<html>");
+            res.write("<p>Order-Number: " + order._id + "</p>");
+            res.write("<p>Status: " + order.state + "</p>");
+            res.write("<form action='/' method='get'><input type='submit' value='Zurueck zum start'></form>");
+            res.end("</html>");
+        });
     };
 }
 
--- routes/index-routes.js
+++ routes/index-routes.js
@@ -3,6 +3,10 @@ import express from 'express';
 const router = express.Router();
 import {indexController} from '../controller/index-controller.js';
 
-router.get("/", indexController.index.bind(indexController));
+router.get("/", indexController.showIndex);
+router.get("/orders", indexController.createOrder);
+router.post("/orders", indexController.createPizza);
+router.get("/orders/:id/", indexController.showOrder);
+router.delete("/orders/:id/", indexController.deleteOrder);
 
 export const indexRoutes = router;
