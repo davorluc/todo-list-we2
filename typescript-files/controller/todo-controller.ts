c51724213f0e52f0ac76da5a4b01d7c1fb7a9cdd
davorluc <d.lucic4@hotmail.com>
2023-05-31 23:52:39 +0200

    star with conversion to typescript

---
+++ typescript-files/controller/todo-controller.ts
@@ -0,0 +1,12 @@
+import {Response} from 'express';
+import {todoService} from '../services/todoService';
+
+export class IndexController {
+
+    renderIndex = async (req:any, res:Response) => {
+        res.render('index', {title: 'Todos Overview', result: {noteList: await todoService.all(req.userSettings.orderBy, req.userSettings.orderDirection, req.userSettings.showCompleted)}, userSettings: req.userSettings});
+    };
+}
+
+export const indexController = new IndexController();
+
